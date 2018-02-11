41182f0df0b18b2b9757d9f048edf6bc617556ea
adnanrahic <ado.raha198@gmail.com>
2018-02-11 23:04:45 +0100

    added single command creation

--- install-kubernetes-worker.sh
+++ install-kubernetes-worker.sh
@@ -13,8 +13,23 @@ EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 
-# replace with the PUBLIC IP address of the worker node
-export WORKER_IP=<worker_ip>
+# this command will grab the public ip of the worker node
+export WORKER_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 # run the join command returned from the init command you ran on the master node
+kubeadm join --token <token> <master_ip>:6443 --discovery-token-ca-cert-hash sha256:<token_hash>
+
+################################################################################################
+
+# OR - use this as a single command
+apt-get update &&
+apt-get install -y docker.io &&
+apt-get update && apt-get install -y apt-transport-https &&
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update &&
+apt-get install -y kubelet kubeadm kubectl &&
+export WORKER_IP=$(dig +short myip.opendns.com @resolver1.opendns.com) &&
 kubeadm join --token <token> <master_ip>:6443 --discovery-token-ca-cert-hash sha256:<token_hash>
