525ac26ef85ab7eb9b2f8abf87ae7f2e73dfc354
adnanrahic <ado.raha198@gmail.com>
2018-02-15 21:18:23 +0100

    (automation) added single-command scripts to init master and worker nodes for kubernetes;

--- install-kubernetes-worker.sh
+++ install-kubernetes-worker.sh
@@ -21,15 +21,4 @@ kubeadm join --token <token> <master_ip>:6443 --discovery-token-ca-cert-hash sha
 
 ################################################################################################
 
-# OR - use this as a single command
-apt-get update &&
-apt-get install -y docker.io &&
-apt-get update && apt-get install -y apt-transport-https &&
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update &&
-apt-get install -y kubelet kubeadm kubectl &&
-export WORKER_IP=$(dig +short myip.opendns.com @resolver1.opendns.com) &&
-kubeadm join --token <token> <master_ip>:6443 --discovery-token-ca-cert-hash sha256:<token_hash>
+# OR - use init_kube_worker.sh as a single command
